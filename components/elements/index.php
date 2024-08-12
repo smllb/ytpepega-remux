4574d25ee57245bbf42ed7b9d253e8d54d78390a
smllb <samuelbarbosa.ipms@gmail.com>
2024-08-11 21:13:23 -0300

    feat: add layout on index.php component and some forms

---
+++ components/elements/index.php
@@ -0,0 +1,34 @@
+<div class="main wrapper">
+    <div class="top header">
+
+        <form method="post" id="submit_url_form">
+            <div class="url_form">
+                <img src="" alt="ytpepegalogo">
+                <input type="text"  class="url_form_input" id="user_url_input" name="user_url_input">
+                <input type="submit" value="submit url...">
+            </div>
+        </form>
+
+    </div>
+    <div class="middle content">
+<!--        content here... videos, query result etc.. -->
+    </div>
+    <div class="bottom footer">
+
+        <form method="post" id="output_format_form">
+            <div class="output_format_form">
+                <label for="output_format">output format</label>
+                <select id="output_format" name="output_format" required>
+                    <option value="mp3" selected>mp3</option>
+                    <option value="mp4">mp4</option>
+                    <option value="ogg">ogg</option>
+                    <option value="vorbis">vorbis</option>
+                </select>
+                <input type="submit" value="set output format">
+            </div>
+        </form>
+
+    </div>
+</div>
+
+
